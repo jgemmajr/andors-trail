cd764d4c10a30c794f759b109f0c6b4055e31ae9
Nut.andor <nut.andor@posteo.de>
2023-10-14 01:10:56 +0200

    cats

--- AndorsTrail/app/src/main/java/com/gpl/rpg/AndorsTrail/resource/ResourceLoader.java
+++ AndorsTrail/app/src/main/java/com/gpl/rpg/AndorsTrail/resource/ResourceLoader.java
@@ -378,6 +378,7 @@ public final class ResourceLoader {
 		loader.prepareTileset(R.drawable.monsters_bosses_2x2, "monsters_bosses_2x2", sz1x1, sz2x2, mTileSize);
 		loader.prepareTileset(R.drawable.monsters_omi2, "monsters_omi2", sz8x3, sz1x1, mTileSize);
 		loader.prepareTileset(R.drawable.monsters_phoenix01, "monsters_phoenix01", sz16x8, sz1x1, mTileSize);
+		loader.prepareTileset(R.drawable.monsters_cats, "monsters_cats", new Size(10, 2), sz1x1, mTileSize);
 		/*INSERT_NPCS_TILESETS_HERE*/
 		
 		loader.prepareTileset(R.drawable.map_bed_1, "map_bed_1", mapTileSize, sz1x1, mTileSize);
