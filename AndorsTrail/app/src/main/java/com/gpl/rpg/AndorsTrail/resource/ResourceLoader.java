edd79b4e4e20599fb006050d621f6676c7760fab
Nut.andor <nut.andor@posteo.de>
2023-10-30 01:23:30 +0100

    new sprite of Omicron

--- AndorsTrail/app/src/main/java/com/gpl/rpg/AndorsTrail/resource/ResourceLoader.java
+++ AndorsTrail/app/src/main/java/com/gpl/rpg/AndorsTrail/resource/ResourceLoader.java
@@ -400,6 +400,7 @@ public final class ResourceLoader {
 		loader.prepareTileset(R.drawable.map_curtain_1, "map_curtain_1", mapTileSize, sz1x1, mTileSize);
 		loader.prepareTileset(R.drawable.map_entrance_1, "map_entrance_1", mapTileSize, sz1x1, mTileSize);
 		loader.prepareTileset(R.drawable.map_entrance_2, "map_entrance_2", mapTileSize, sz1x1, mTileSize);
+		loader.prepareTileset(R.drawable.map_entrance_3, "map_entrance_3", mapTileSize, sz1x1, mTileSize);
 		loader.prepareTileset(R.drawable.map_fence_1, "map_fence_1", mapTileSize, sz1x1, mTileSize);
 		loader.prepareTileset(R.drawable.map_fence_2, "map_fence_2", mapTileSize, sz1x1, mTileSize);
 		loader.prepareTileset(R.drawable.map_fence_3, "map_fence_3", mapTileSize, sz1x1, mTileSize);
